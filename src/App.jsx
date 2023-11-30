14d64a12b987ea03ca58342656962c09c6d708d5
Nati Lavega <nati.lavega@gmail.com>
2023-11-30 15:26:46 -0300

    feat: muestra input de busqueda y datos mock de clima

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
+import responseWeather from './mocks/with-results.json'
+import withoutResults from './mocks/no-results.json'
 import './App.css'
 
 function App () {
+  const { location, current: weather }= responseWeather
+
   return (
     <>
-      <h1>Weather App</h1>
-    </>
+      <header>
+        <form className='form'>
+          <input placeholder='Buenos Aires, Nueva York, Londres...' />
+          <button>Buscar</button>
+        </form>
+      </header>
+      <main>
+        {
+          location ? (
+            <div className='location'>
+              <p><strong>{ location.name }</strong>, { location.region }, { location.country }</p>
+            </div>
+          ) : (null)
+        }
+        {
+          weather ? (
+            <div className='weather'>
+              <div>
+                <img src={ weather.condition.icon } alt={ weather.condition.text } />
+                <h3>{ weather.temp_c }°c</h3>
+              </div>
+              <p>{ weather.condition.text }</p>
+            </div>
+          ) : (
+            <p>No se encontraron resultados.</p>
+          )
+        }
+      </main>
+    </> 
   )
 }
 
