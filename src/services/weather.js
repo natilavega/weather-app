916cbd68ab09144915291f2e1905cee46680b89f
Nati Lavega <nati.lavega@gmail.com>
2023-12-07 11:28:36 -0300

    chore: cambia protocolo de transferencia de datos

--- src/services/weather.js
+++ src/services/weather.js
@@ -1,5 +1,5 @@
 const API_KEY = import.meta.env.VITE_API_KEY
-const WEATHER_ENDPOINT_BASE = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&lang=es`
+const WEATHER_ENDPOINT_BASE = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&lang=es`
 
 export async function searchWeather ( { search } ) {
   if ( search === '' ) return ( {} )
